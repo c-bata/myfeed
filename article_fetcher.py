92fec48588d30486a40ea3d7f405a3c0ee386dfb
c-bata <contact@c-bata.link>
2016-05-23 23:54:05 +0900

    Counting words frequency

--- article_fetcher.py
+++ article_fetcher.py
@@ -1,13 +1,39 @@
-from feedy import Feedy
 import os
+from collections import Counter
+
+from feedy import Feedy
+from bs4 import BeautifulSoup
+from janome.tokenizer import Tokenizer
 
 BASE_DIR = os.path.dirname(__file__)
 feedy = Feedy(os.path.join(BASE_DIR, 'feedy.dat'))
 
+t = Tokenizer()
+
+
+def _get_proper_nouns(text):
+    return [token.surface for token in t.tokenize(text)
+            if token.part_of_speech.startswith('名詞,固有名詞')]
+
+
+def get_words_from_body(body):
+    word_counter = Counter()
+    soup = BeautifulSoup(body, "html.parser")
+    for child_tag in soup.find('body').findChildren():
+        if child_tag.name == 'script':
+            continue
+        child_text = child_tag.text
+        for line in child_text.split('\n'):
+            line = line.rstrip().lstrip()
+            words = _get_proper_nouns(line)
+            if words:
+                word_counter.update(words)
+    return word_counter
+
 
 @feedy.add('http://b.hatena.ne.jp/hotentry/it.rss')
 def hatena_it(feed_info, entry_info, body):
-    print(entry_info['title'])
+    print(get_words_from_body(body))
 
 
 @feedy.add('http://hnrss.org/newest?points=50')
